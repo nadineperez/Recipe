98f5a9b00793aa6d0c5c955fdd974df5d0e98e6e
Adam Dachis <nadineperez96@gmail.com>
2018-12-03 16:48:11 -0500

    add tester.php

--- php/generate.php
+++ php/generate.php
@@ -1,1 +1,26 @@
+<?php
+$servername = "localhost";
+$username = "username";
+$password = "password";
+$dbname = "myDB";
 
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT ingredient_name FROM ingredient";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "ingredient name: " . $row["ingredient_name"];
+    }
+} else {
+    echo "0 results";
+}
+$conn->close();
+?>
