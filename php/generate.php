a79c31c72a24fd08f72478fffca68aad07bd3724
Adam Dachis <nadineperez96@gmail.com>
2018-12-04 17:40:31 -0500

    new connection test

--- php/generate.php
+++ php/generate.php
@@ -1,11 +1,7 @@
 <?php
-$servername = "localhost";
-$username = "username";
-$password = "password";
-$dbname = "myDB";
-
 // Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = new mysqli('localhost', 'root', '', 'Recipe');
+
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
