8c0e80347bc8660fc0b8e0c7a6b3d92a697dc75b
Adam Dachis <nadineperez96@gmail.com>
2018-12-13 18:01:36 -0500

    fix parentheses

--- php/generate.php
+++ php/generate.php
@@ -8,15 +8,12 @@ $conn = new mysqli('localhost', 'root', 'inst377', 'Recipedatabase');
 if ($conn->connect_error) die("Connection failed: " . $conn->connect_error());
 $result = $conn->query($namequery);
 
-//$sql = "SELECT ingredient_name FROM ingredient";
-// $result = $conn->query($namequery);
-//
 if ($result->num_rows > 0) {
     // output data of each row
     while($row = $result->fetch_assoc()) {
         echo "ingredient name: " . $row["ingredient_name"];
     }
-
+}
    // echo $namequery;
 
 $conn->close();
