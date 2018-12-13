e9e83438ee639f5c66180bc95cccb3cd6c3c819e
Adam Dachis <nadineperez96@gmail.com>
2018-12-13 17:22:38 -0500

    fetch ing

--- php/generate.php
+++ php/generate.php
@@ -1,21 +1,23 @@
 <?php
+
+$namequery = "SELECT ingredient_name FROM ingredient WHERE ingredient_name = ". $_GET['tid'];
 // Create connection
 $conn = new mysqli('localhost', 'root', 'inst377', 'Recipedatabase');
-$sql = "SELECT ingredient_name FROM ingredient";
 
 // Check connection
 if ($conn->connect_error) die("Connection failed: " . $conn->connect_error());
+$result = $conn->query($namequery);
 
 //$sql = "SELECT ingredient_name FROM ingredient";
-$result = $conn->query($sql);
+// $result = $conn->query($namequery);
+//
+// if ($result->num_rows > 0) {
+//     // output data of each row
+//     while($row = $result->fetch_assoc()) {
+//         echo "ingredient name: " . $row["ingredient_name"];
+//     }
 
-if ($result->num_rows > 0) {
-    // output data of each row
-    while($row = $result->fetch_assoc()) {
-        echo "ingredient name: " . $row["ingredient_name"];
-    }
-} else {
-    echo "0 results";
-}
+    echo $result = $conn->query($namequery);;
+
 $conn->close();
 ?>
