80f0ebb24d45b6c5f395ff9bdd01e3afbb8b5c6c
Adam Dachis <nadineperez96@gmail.com>
2018-12-14 09:12:49 -0500

    finishing touches

--- php/generate.php
+++ php/generate.php
@@ -56,18 +56,15 @@ for ($x = 0; $x <= count($required_ingredients); $x++) {
 
 print_r($recipes_to_suggest) . "<br />";
 
-
 while($row = $valid_recipe->fetch_assoc()) {
   $id = $row["recipe_id"];
   $name = $row["recipe_name"];
 
   for ($x = 0; $x <= count($recipes_to_suggest); $x++) {
-
-     if ($recipes_to_suggest == $id) {
-     array_push($recipe_names, $name);
-  }
+     if ($recipes_to_suggest[$x] == $id) {
+        array_push($recipe_names, $name);
+     }
  }
-
 }
 
 echo "Recipes to suggest: ";
