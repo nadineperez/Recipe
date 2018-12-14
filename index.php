ae567ab3afd3b17f2fb50a0033a9535b03498685
Adam Dachis <nadineperez96@gmail.com>
2018-12-13 22:00:21 -0500

    post to get

--- index.php
+++ index.php
@@ -1,14 +1,6 @@
 <?php
    session_start();
    //$conn = new mysqli('localhost', 'root', 'inst377', 'Recipedatabase');
-
-// <!DOCTYPE HTML>
-// <!--
-// Editorial by HTML5 UP
-// html5up.net | @ajlkn
-// Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
-// -->
-// <!-- jQuery library -->
 ?>
 <html>
 <head>
@@ -61,6 +53,8 @@
                      $_SESSION["favcolor"] = "myIngredient";
                      $_SESSION["favanimal"] = myIngredient;
                      echo "Session variables are set.";
+                     $list = $_GET['ingredientList'];
+                     echo $list;
                   ?>
               </div>
 
--- php/generate.php
+++ php/generate.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 $namequery = "SELECT ingredient_name FROM ingredient WHERE ingredient_id = ". $_GET['ing'];
-$list = $_POST['ingredientList'];
+$list = $_GET['ingredientList'];
 // Create connection
 $conn = new mysqli('localhost', 'root', 'inst377', 'Recipedatabase');
 
