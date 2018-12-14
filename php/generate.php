d0f27c9ea8d7ce124562782b57508b94733383ee
Adam Dachis <nadineperez96@gmail.com>
2018-12-14 01:12:12 -0500

    add qu

--- php/generate.php
+++ php/generate.php
@@ -3,24 +3,32 @@ session_start();
 
 // $namequery = "SELECT ingredient_name FROM ingredient WHERE ingredient_id = ". $_GET['ing'];
 // $ingredientId = "SELECT * FROM recipe_ingredient WHERE ingredient_id = ". $_GET['ing'];
+
 $list = $_SESSION["ingredientList"];
+$ings = "SELECT * FROM ingredient JOIN recipe_ingredient ON ingredient_id";
+$joined = "SELECT * FROM recipe JOIN $ings ON recipe_id";
+$recipes = "SELECT * FROM $joined WHERE ingredient IN ($list)"
 
-foreach ($list as $ingredient) {
-    //$ingid = "SELECT ingredient_id FROM ingredient WHERE ingredient_name =" $ingredient;
-    //$associated_recipes = "SELECT * FROM recipe_ingredient WHERE ingredient_id =" $ingid;
-    $name = "SELECT ingredient_id FROM ingredient WHERE ingredient_name = ". $ingredient;
-
-    echo "Ingredient is " . $ingredient . ".<br>";
-    echo "Ingredient name is " . $name . ".<br>";
-
-}
+//recipes = "SELECT * FROM $joined WHERE ingredient IN ($list)";
+// foreach ($list as $ingredient) {
+//     //$ingid = "SELECT ingredient_id FROM ingredient WHERE ingredient_name =" $ingredient;
+//     //$associated_recipes = "SELECT * FROM recipe_ingredient WHERE ingredient_id =" $ingid;
+//     $name = "SELECT ingredient_id FROM ingredient WHERE ingredient_name = ". $ingredient;
+//
+//     echo "Ingredient is " . $ingredient . ".<br>";
+//     echo "Ingredient name is " . $name . ".<br>";
+//
+// }
 
 //session_unset();
 // Create connection
 $conn = new mysqli('localhost', 'root', 'inst377', 'Recipedatabase');
-
 // Check connection
 if ($conn->connect_error) die("Connection failed: " . $conn->connect_error());
+
+$recipes = $conn->query($recipes);
+$matches = array($recipes);
+
 
 // if ($ingredientId->num_rows > 0) {
 //     // output data of each row
