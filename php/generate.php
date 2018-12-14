0a36506cf9f89af252bcb285568972dc9bd67c10
Adam Dachis <nadineperez96@gmail.com>
2018-12-14 01:43:41 -0500

    add qu

--- php/generate.php
+++ php/generate.php
@@ -6,11 +6,10 @@ session_start();
 
 $list = $_SESSION["ingredientList"];
 $condition = implode(', ', $list);
-echo $condition;
 
 $ings = "SELECT * FROM ingredient JOIN recipe_ingredient ON ingredient_id";
 $joined = "SELECT * FROM recipe JOIN $ings ON recipe_id";
-$recipes = "SELECT * FROM $joined WHERE ingredient IN ($condition)";
+$recipes = "SELECT recipe_name FROM $joined WHERE ingredient IN ($condition)";
 
 //recipes = "SELECT * FROM ". $joined WHERE ingredient IN ". $list)";
 // foreach ($list as $ingredient) {
@@ -30,6 +29,9 @@ $conn = new mysqli('localhost', 'root', 'inst377', 'Recipedatabase');
 if ($conn->connect_error) die("Connection failed: " . $conn->connect_error());
 
 $step1 = $conn->query($ings);
+$num1=$mysqli->mysqli_num_rows($step1);
+echo $num1
+
 $finaltable = $conn->query($joined);
 $recipes = $conn->query($recipes);
 $matches = array($recipes);
@@ -40,26 +42,6 @@ if ($recipes->num_rows > 0) {
         echo "recipe id: " . $row["recipe_id"] . ".<br>";
     }
 }
-
-
-
-//$recipes = $conn->query($ingredientId);
-
-// if ($ingredient->num_rows > 0) {
-//     // output data of each row
-//     while($row = $ingredient->fetch_assoc()) {
-//         echo "ingredient name: " . $row["ingredient_name"] . ".<br>";
-//     }
-// }
-
-// if ($recipes->num_rows > 0) {
-//     // output data of each row
-//     while($row = $recipes->fetch_assoc()) {
-//         echo "associated recipe: " . $row["recipe_id"] . ".<br>";
-//     }
-// }
-
-
 
 //print_r($_SESSION);
 
