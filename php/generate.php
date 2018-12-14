4134ae04b640f065eff21dd14bdf6e65b6ce400a
Adam Dachis <nadineperez96@gmail.com>
2018-12-14 05:18:50 -0500

    add qu

--- php/generate.php
+++ php/generate.php
@@ -5,26 +5,12 @@ session_start();
 // $ingredientId = "SELECT * FROM recipe_ingredient WHERE ingredient_id = ". $_GET['ing'];
 
 $list = $_SESSION["ingredientList"];
+
 $valid = "SELECT recipe_name FROM Recipe";
 
 $table = "SELECT * FROM Recipe
 LEFT JOIN recipe_ingredient ON Recipe.recipe_id=recipe_ingredient.recipe_id
 LEFT JOIN ingredient ON ingredient.ingredient_id=ingredient.ingredient_id";
-//WHERE ingredient.ingredient_name IN ($condition)";
-//LEFT JOIN recipe_ingredient ON ingredient_id";
-//$joined = "SELECT * FROM recipe JOIN $ings ON recipe_id";
-//$recipes = "SELECT recipe_name FROM $joined WHERE ingredient IN ($condition)";
-
-//recipes = "SELECT * FROM ". $joined WHERE ingredient IN ". $list)";
-// foreach ($list as $ingredient) {
-//     //$ingid = "SELECT ingredient_id FROM ingredient WHERE ingredient_name =" $ingredient;
-//     //$associated_recipes = "SELECT * FROM recipe_ingredient WHERE ingredient_id =" $ingid;
-//     $name = "SELECT ingredient_id FROM ingredient WHERE ingredient_name = ". $ingredient;
-//
-//     echo "Ingredient is " . $ingredient . ".<br>";
-//     echo "Ingredient name is " . $name . ".<br>";
-//
-// }
 
 //session_unset();
 // Create connection
@@ -44,36 +30,16 @@ $uniquerecipes = array_unique($recipearray);
 if ($step->num_rows > 0) {
     // output data of each row
     while($row = $step->fetch_assoc()) {
-      //echo $row["ingredient_name"];
-        // if (in_array($row["ingredient_name"], $list)) {
-        //    echo "recipe name: " . $row["recipe_name"] . " ingredient id: " . $row["ingredient_name"] . ".<br>";
-        // }
+
         $ingredient = $row["ingredient_name"];
         $recipe_name = $row["recipe_name"];
 
-        echo gettype($uniquerecipes);
-        echo "list is " . gettype($list);
-
         if (in_array($ingredient, $list) == FALSE) {
            unset($uniquerecipes[$recipe_name]);
-           //echo $recipe_name;
+           $uniquerecipes= array_flip($uniquerecipes)
         }
     }
 }
-//print_r($uniquerecipes);
-
-// $finaltable = $conn->query($joined);
-// $recipes = $conn->query($recipes);
-// $matches = array($recipes);
-//
-// if ($recipes->num_rows > 0) {
-//     // output data of each row
-//     while($row = $recipes->fetch_assoc()) {
-//         echo "recipe id: " . $row["recipe_id"] . ".<br>";
-//     }
-// }
-
-//print_r($_SESSION);
-
+print_r($uniquerecipes);
 $conn->close();
 ?>
