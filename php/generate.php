65ae69b527331281c4b07504150c15b832a8601f
Adam Dachis <nadineperez96@gmail.com>
2018-12-14 00:35:19 -0500

    add qu

--- php/generate.php
+++ php/generate.php
@@ -4,13 +4,15 @@ session_start();
 // $namequery = "SELECT ingredient_name FROM ingredient WHERE ingredient_id = ". $_GET['ing'];
 // $ingredientId = "SELECT * FROM recipe_ingredient WHERE ingredient_id = ". $_GET['ing'];
 $list = $_SESSION["ingredientList"];
+
 foreach ($list as $ingredient) {
-    $ingid = "SELECT ingredient_id FROM ingredient WHERE ingredient_name =" $ingredient;
-    $associated_recipes = "SELECT * FROM recipe_ingredient WHERE ingredient_id =" $ingid;
+    //$ingid = "SELECT ingredient_id FROM ingredient WHERE ingredient_name =" $ingredient;
+    //$associated_recipes = "SELECT * FROM recipe_ingredient WHERE ingredient_id =" $ingid;
+    $name = "SELECT ingredient_id FROM ingredient WHERE ingredient_name = " $ingredient;
 
     echo "Ingredient is " . $ingredient . ".<br>";
-    echo "Ingredient id is " . $ingid . ".<br>";
-    echo "associated_recipes is " . $associated_recipes . ".<br>";
+    echo "Ingredient name is " . $name . ".<br>";
+
 }
 
 //session_unset();
@@ -20,12 +22,12 @@ $conn = new mysqli('localhost', 'root', 'inst377', 'Recipedatabase');
 // Check connection
 if ($conn->connect_error) die("Connection failed: " . $conn->connect_error());
 
-if ($associated_recipes->num_rows > 0) {
-    // output data of each row
-    while($row = $associated_recipes->fetch_assoc()) {
-        echo "recipe id: " . $row["recipe_id"] . ".<br>";
-    }
-}
+// if ($ingredientId->num_rows > 0) {
+//     // output data of each row
+//     while($row = $associated_recipes->fetch_assoc()) {
+//         echo "recipe id: " . $row["recipe_id"] . ".<br>";
+//     }
+// }
 
 
 
